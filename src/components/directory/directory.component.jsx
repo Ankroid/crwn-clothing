22b30d1efcd6c2de9f632a4172e082f1e6c4c685
Ankur Badola <ankur.badola@kony.com>
2020-01-20 19:20:18 +0530

    update our menu-item to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ export default class Directory extends React.Component {
                 {
                     title: "hats",
                     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-                    id: 1
+                    id: 1,
+                    linkURL:'hats'
                 },
                 {
                     title: "jackets",
                     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-                    id: 2
+                    id: 2,
+                    linkURL:''
                 },
                 {
                     title: "sneakers",
                     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-                    id: 3
+                    id: 3,
+                    linkURL:''
                 },
                 {
                     title: 'womens',
                     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
                     id: 4,
-                    size :'large'
+                    size :'large',
+                    linkURL:''
                 },
                 {
                     title: 'mens',
                     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
                     id: 5,
-                    size :'large'
+                    size :'large',
+                    linkURL:''
                 }
             ]
         }
@@ -40,8 +45,11 @@ export default class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-               {this.state.section.map(({title,imageUrl,id,size})=>(
-                   <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+               {/* {this.state.section.map(({title,imageUrl,id,size,linkURL})=>(
+                   <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkURL={linkURL}/>
+               ))} */}
+               {this.state.section.map(({id,...otherSectionProps})=>(
+                   <MenuItem key={id} {...otherSectionProps}/>
                ))}
             </div>
         )
